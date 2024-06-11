09607d673244a038847ebc477f50527d811634b9
Scott Cowan <scott.cowan@marketing.delivery>
2024-06-12 00:15:49 +0100

    adding terraform and github actions

--- README.md
+++ README.md
@@ -18,6 +18,18 @@ First, run `pnpm i` to install the dependencies.
 
 Then, run `pnpm dev` to start the development server and visit localhost:3000.
 
+## Setup deployments
+
+1. Add permissions for github actions to run in your aws account
+    1. [Create a role for OpenID Connect federation (console)](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-idp_oidc.html#idp_oidc_Create_GitHub)
+1. Add and validate a certificate in the AWS Certificate Manager in the N. Virginia region for your domain (for use with CloudFront)
+1. Add github variables for
+    1. **AWS_ROLE_TO_ASSUME** : ARN for the role created in the first step
+    1. **AWS_TERRAFORM_BUCKET** : name of the S3 bucket in AWS used for storing terraform state
+    1. **DOMAIN** : domain used to find the certificate to associate with cloudfront and used for the name of the content bucket
+    1. **WEB_ACL_ARN** (optional) : ARN for any WAF rules used for whitelisting IPs
+    1. **AWS_REGION** : (eg: eu-west-1) used to host the content bucket and needs to be the same region as the terraform bucket. the cloudfront will always hosted in us-east-1
+1. Add a CNAME to point to the cloudfront url for your domain.
 ## License
 
 This project is licensed under the MIT License.
---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.32.1"
+    }
+  }
+
+  required_version = "~> 1.8.0"
+  backend "s3" {}
+}
+
+variable "domain" {
+  description = "url to use for the tfstate file and cloudfront distribution (eg: site.acme.com)"
+}
+
+variable "region" {
+  description = "the distribution will be created in us-east-1 but bucket will be in the region specified here"
+}
+
+variable "source_path" {
+  description = "the location of the files to be uploaded"
+  default = "./out"
+}
+
+variable "web_acl_arn" {
+  description = "the arn of the web ACL"
+  default = null
+}
+
+locals {
+  default_tags = {
+    App = var.domain
+    ManagedByTerraform = "True"
+  }
+}
+
+module "site_files" {
+  source      = "github.com/marketing-delivery/terraform-modules//modules/s3-web-bucket"
+  name        = var.domain
+  tags        = local.default_tags
+}
+
+module "bucket_upload" {
+  source      = "github.com/marketing-delivery/terraform-modules//modules/s3-upload"
+  bucket_id = module.site_files.id
+  source_path = var.source_path 
+}
+
+module "cloudfront_distribution" {
+  source = "github.com/marketing-delivery/terraform-modules//modules/cloudfront"
+  domain = var.domain
+  bucket_id = module.site_files.id
+  bucket_arn = module.site_files.bucket_arn
+  bucket_regional_domain_name = module.site_files.bucket_regional_domain_name
+  web_acl_id = var.web_acl_arn
+  tags = local.default_tags
+}
+
+output "distribution_id" {
+  value = module.cloudfront_distribution.id
+}
