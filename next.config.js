f8b6b19d822e7d807b3776ce4a18276cba53f844
Scott Cowan <scott.cowan@marketing.delivery>
2024-06-12 00:32:44 +0100

    fixing config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const { imageOptimizer } = require('next/dist/server/image-optimizer')
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.tsx'
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: false
+})
